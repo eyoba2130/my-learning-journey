566991bd567412cd42ecdbf11830725633f06885
eyob21-cs <eyobtsegaw8@gmail.com>
2026-09-02 09:47:43 +0300

    feat:(add to package and index js ) index js to how work res, req, url, mehtod, logger to middwilere and error handler function

---
+++ 01. AI Full-Stack Web Developer Course/01. AI Full-Stack Web Developer Course/02.AI_Powered_Application_Architecture_&_Building_the_Chat/01.ChatGPT Clone Backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+const app = express();
+
+
+function logger(req, res, next) {
+    const url = req.url;
+    const method = req.method;
+    console.log(url, method);
+    next();
+}
+
+function loggerSecond(req, res, next) {
+  
+   console.log("Second middleware:");
+    next();
+     
+}
+
+app.use("/api", logger);
+
+
+app.get("/",(req, res) => {
+    res.send("Hello World for home page");
+});
+
+app.get("/about", (req, res) => {
+    res.send("Hello World for about page");
+});
+
+app.get("/api/chat", (req, res) => {
+    res.send("Hello World for route chat");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+})
