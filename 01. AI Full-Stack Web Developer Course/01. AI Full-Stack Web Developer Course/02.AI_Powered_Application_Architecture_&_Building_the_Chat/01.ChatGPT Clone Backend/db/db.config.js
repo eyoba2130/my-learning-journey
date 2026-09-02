d182b2b179d797e7eb95dc53a385c9ecf911f8fb
eyob21-cs <eyobtsegaw8@gmail.com>
2026-09-02 11:21:50 +0300

    (feat:add to db config js and env file)

---
+++ 01. AI Full-Stack Web Developer Course/01. AI Full-Stack Web Developer Course/02.AI_Powered_Application_Architecture_&_Building_the_Chat/01.ChatGPT Clone Backend/db/db.config.js
@@ -0,0 +1,12 @@
+import mysql from "mysql2/promise";
+
+const dbConfig = {
+    host: process.env.DB_HOST ,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME 
+}
+
+const db = mysql.createConnection(dbConfig);
+
+export default db;
