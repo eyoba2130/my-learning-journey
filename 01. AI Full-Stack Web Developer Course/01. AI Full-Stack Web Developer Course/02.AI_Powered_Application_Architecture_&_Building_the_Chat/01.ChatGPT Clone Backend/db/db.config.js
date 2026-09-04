3763244232ce15855ccc2e204e2586e89609ead5
eyob21-cs <eyobtsegaw8@gmail.com>
2026-09-04 20:44:05 +0300

    fix:(db.config.js) change create mysql to create fix the server error

--- 01. AI Full-Stack Web Developer Course/01. AI Full-Stack Web Developer Course/02.AI_Powered_Application_Architecture_&_Building_the_Chat/01.ChatGPT Clone Backend/db/db.config.js
+++ 01. AI Full-Stack Web Developer Course/01. AI Full-Stack Web Developer Course/02.AI_Powered_Application_Architecture_&_Building_the_Chat/01.ChatGPT Clone Backend/db/db.config.js
@@ -3,15 +3,17 @@ import mysql from "mysql2/promise";
 
 
 
-const dbConfig = {
+const db = await mysql.createPool({
+
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-};
+  connectionLimit: 10,
+  
+}
+  
+);
 
-const db = await mysql.createConnection(dbConfig);
-
-console.log("Database connected successfully!");
 
 export default db;
