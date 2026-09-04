80847232920257f5b3b5ca1e23a36d7f22784761
eyob21-cs <eyobtsegaw8@gmail.com>
2026-09-04 20:49:31 +0300

    feat: add new functionality to registration form import mainRouter

--- 01. AI Full-Stack Web Developer Course/01. AI Full-Stack Web Developer Course/02.AI_Powered_Application_Architecture_&_Building_the_Chat/01.ChatGPT Clone Backend/server.js
+++ 01. AI Full-Stack Web Developer Course/01. AI Full-Stack Web Developer Course/02.AI_Powered_Application_Architecture_&_Building_the_Chat/01.ChatGPT Clone Backend/server.js
@@ -1,29 +1,31 @@
 
 import express from "express";
 import db from "./db/db.config.js";
-
+import mainRouter from "./src/api/main.routes.js"
 const app = express();
-app.post("/api/conversations", async (req, res) => {
-  res.send("post method is working");
-});
 
-app.get("/api/conversations", async (req, res) => {
-res.send("get method is working");
-});
+app.use('/api', mainRouter)
+
+// app.post("/api/conversations", async (req, res) => {
+//   res.send("post method is working");
+// });
+
+// app.get("/api/conversations", async (req, res) => {
+// res.send("get method is working");
+// });
 
 
-async function startServer () {
+async function startServer() {
+  
     try {
 
-
       const connection = await db.getConnection();
-      connection.release();
+
+      console.log("db connected")
+      // connection.release();
 
         app.listen(3000, () => {
-          if (err) {
-            throw (err);
-        }
-
+       
         console.log("Server is running on http://localhost:3000");
       
 })
@@ -31,4 +33,5 @@ async function startServer () {
         console.log("Error server starting", error.message);
     }
 }
+
 startServer();
