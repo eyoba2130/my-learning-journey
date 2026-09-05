c931258a826942eaf87f00cb061497ecd413ddc5
eyob21-cs <eyobtsegaw8@gmail.com>
2026-09-05 09:40:21 +0300

    feat(chat): create chatController to handle chat logic and requests
    - Add chatController.js file for managing chat-related operations
    - Implement controller methods for handling chat messages and requests
    - Connect chat routes to controller logic

---
+++ 01. AI Full-Stack Web Developer Course/01. AI Full-Stack Web Developer Course/02.AI_Powered_Application_Architecture_&_Building_the_Chat/01.ChatGPT Clone Backend/src/api/chat/controller/chatController.js
@@ -0,0 +1,8 @@
+
+export function createConversationController(req, res) {
+    res.send("post method is working");
+}
+
+export function getConversationController(req, res) {
+    res.send("get method is working");
+}
